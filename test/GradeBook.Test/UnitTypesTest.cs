0c4dbcb786910839375221f6c737b35c9c962614
michaelgobz <raniahgobz@outlook.com>
2023-05-16 15:12:50 +0300

    class field changes

--- test/GradeBook.Test/UnitTypesTest.cs
+++ test/GradeBook.Test/UnitTypesTest.cs
@@ -14,16 +14,46 @@ namespace GradeBook.Test
             Book book4 = GetBook("book 4");
 
             Assert.Equal("book 1",book1.Name);
-            Assert.Equal("book 2",book2.Name)
+            Assert.Equal("book 2",book2.Name);
             Assert.Equal("book 3",book3.Name);
             Assert.Equal("book 4",book4.Name);
 
         }
 
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            Book book1 = GetBook("book 1");
+            Book book2 = book1;
 
+            Assert.Equal("book 1",book1.Name);
+            Assert.Equal("book 1",book2.Name);
+            Assert.Same(book1, book2);
+        }
+
+        [Fact]
+        public void IsTheChangedNameVisible()
+        {
+            // arrange
+            Book book5 = new Book("book 5");
+            var name = "Michael's GradeBook";
+
+            //act
+            SetName(boo5, name);
+
+            //assert 
+            Assert.Equal(name, book5.Name);
+        }
+
+        // utility methods 
         internal Book GetBook(string name)
         {
             return new Book(name);
+        }
+
+        internal private void SetName(Book b, string name)
+        {
+            b.Name = name;         
         }
     }
 }
