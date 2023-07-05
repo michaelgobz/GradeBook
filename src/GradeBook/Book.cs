cec7b99273a6ed00e6b7bc1d41a1e050d62e14d9
michaelgobz <raniahgobz@outlook.com>
2023-07-05 17:04:42 +0300

    polymohism

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -5,7 +5,6 @@ namespace GradeBook
     public interface IBook
     {
         void AddGrade(double grade);
-        Statistics ComputeStatistics();
         void ShowStatistics(Statistics result);
         event GradeAddDelegate GradeAdded;
         string Name { get; }
@@ -52,58 +51,13 @@ namespace GradeBook
                 throw new ArgumentException($"invalid {nameof(grade)}");
             }
         }
-
-        public void AddLetterGrade(char Letter)
+        public void ShowAndPrintStatistics(Statistics result)
         {
-
-        }
-
-        public Statistics ComputeStatistics()
-        {
-            Statistics result = new Statistics();
-
-            result.maximum = 0.0;
-            result.minimum = 0.0;
-
             foreach (var grade in this.Grades)
             {
-                result.total += grade;
-                result.maximum = Math.Max(grade, result.maximum);
-                result.minimum = Math.Min(grade, result.minimum);
+                result.Add(grade);
             }
 
-            // average
-            result.average = result.total / this.Grades.Count;
-            // compute the letter grade
-
-            switch (result.average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                case var d when d >= 50.0:
-                    result.Letter = 'E';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
-
-            }
-
-            return result;
-        }
-
-        public void ShowStatistics(Statistics result)
-        {
             Console.WriteLine($"For the Book named .... {this.Name}");
             Console.WriteLine($"The total is {result.total:N2}");
             Console.WriteLine($"The average is {result.average:N2}");
--- src/GradeBook/Statisitcs.cs
+++ src/GradeBook/Statisitcs.cs
@@ -2,11 +2,57 @@ namespace GradeBook
 {
     public class Statistics 
     {
-        public double total;
-        public double average;
+        int count = 0;
+        double sum = 0;
+        public double total{
+            get {
+                return sum;
+            }
+        }
+        public double average {
+            get {
+                return sum / count;
+            }
+        }
         public double maximum;
         public double minimum;
-        public char Letter;
+        public char Letter {
+            get {
+                return this.Letter;
+            }
+
+            set {
+                switch (value)
+            {
+                case var d when d >= 90.0:
+                    Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    Letter = 'D';
+                    break;
+                case var d when d >= 50.0:
+                    Letter = 'E';
+                    break;
+                default:
+                    Letter = 'F';
+                    break;
+
+            }
+            }
+        }
+
+        public void Add(double number){
+            sum =+ number;
+            count =+ 1;
+            maximum = Math.Max(number, maximum);
+            minimum = Math.Min(number, minimum);
+        }
 
     }
 }
